17f671e7a035b0efe5f53e02652a4b2006e04c30
Gaurav <gauravpawar3102@gmail.com>
2022-01-20 21:09:06 +0530

    ta

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Navbar() {
   return (
     <div className="container w-full">
-      <div className="left-section">Gaia</div>
+      <div className="left-section">Gaia l</div>
       <div className="right-section">
         <div className="menu"></div>
       </div>
